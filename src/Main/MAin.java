608e67161ef7d3f670af4d91163844a30671991b
Hp <rishabhrai980@gmail.com>
2020-04-27 11:13:12 +0530

    this merge sorting are complete

---
+++ src/Main/MAin.java
@@ -0,0 +1,21 @@
+package Main;
+import mergesort.MergeSort;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MAin {
+    public static void main(String[] args) {
+        System.out.println("How many elements would you like in the array?: ");
+        final Scanner scanner = new Scanner(System.in);
+        int numberOfElements = scanner.nextInt();
+        int[] array = new int[numberOfElements];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 100);
+        }
+        MergeSort.sort(array);
+        System.out.println("ARRAY SORTED BY MERGE SORT");
+        System.out.println(Arrays.toString(array));
+    }
+
+}
